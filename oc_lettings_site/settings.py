d4575e8209822e018d652b0e5b6f1f982a85f52f
Thomas Jeanne <thjbox@gmail.com>
2025-05-22 14:12:29 +0200

    Docker config, test push

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,4 +1,4 @@
-import os, sys
+import os
 import sentry_sdk
 import logging
 from sentry_sdk.integrations.django import DjangoIntegration
@@ -140,9 +140,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static", ]
-#if 'test' in sys.argv:
-    #STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-    
+
 # Logging Configuration
 LOGGING = {
     'version': 1,
